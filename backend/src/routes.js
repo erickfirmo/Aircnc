5b0f3aac1c377edcfd23013120b5558e888de023
Erick <erickluques@gmail.com>
2019-12-02 02:40:55 -0200

    Update routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,7 +9,7 @@ const routes = express.Router();
 const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store);
-routes.post('/spots', upload.single('thumbnail'). SpotController.store);
+routes.post('/spots', upload.single('thumbnail'), SpotController.store);
 
 
 module.exports = routes;
