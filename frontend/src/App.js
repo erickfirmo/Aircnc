6fd9c1bac72e565372d439e1d4769b7ecb64dbd4
erickfirmo <erickluques@gmail.com>
2019-12-11 16:12:15 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ function App() {
     event.preventDefault();
     const response = await api.post('/sessions', { email });
     const { _id } = response.data;
+    localStorage.setItem('user', _id);
   }
 
   return (
