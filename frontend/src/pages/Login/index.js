9a2e97b9c4d85dae9a7d4ff8edc0564058e3252b
erickfirmo <erickluques@gmail.com>
2019-12-19 13:51:42 -0300

    Update index.js

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import api from '../../services/api';
 
-export default function Login() {
+export default function Login({ history }) {
 
     const [email, setEmail] = useState('');
 
@@ -10,6 +10,8 @@ export default function Login() {
         const response = await api.post('/sessions', { email });
         const { _id } = response.data;
         localStorage.setItem('user', _id);
+
+        history.push('/dashboard');
     }
 
     return (
