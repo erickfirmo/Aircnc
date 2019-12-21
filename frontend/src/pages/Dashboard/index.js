0c64768d520d36a937efc46508a936cb9a43b110
Erick <erickluques@gmail.com>
2019-12-21 00:36:23 -0200

    Update index.js

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -23,7 +23,7 @@ export default function Dashboard() {
             <ul className="spot-list">
                 {spots.map(spot => (
                     <li key={spot._id}>
-                        <header />
+                        <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }}/>
                         <strong>{spot.company}</strong>
                         <span>{spot.price}</span>
                     </li>
