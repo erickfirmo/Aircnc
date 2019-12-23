84ef6ab5c4baa1cba9a9e5e446f049a1cc36b6ff
Erick <erickluques@gmail.com>
2019-12-23 16:36:14 -0200

    Update index.js

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function New() {
-    return <div />
+    const [company, setCompany] = useState('');
+    const [techs, setTechs] = useState('');
+
+
+
+    function handleSubmit() {
+
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="company">EMPRESA *</label>
+            <input
+                id="company"
+                placeholder="Sua empresa incrível"
+                value={company}
+                onChange={event => setCompany(event.target.value)}        
+            />
+
+            <label htmlFor="techs">TECNOLOGIAS *</label>
+            <input
+                id="techs"
+                placeholder="Quais tecnologias usam?"
+                value={techs}
+                onChange={event => setTechs(event.target.value)}        
+            />
+
+        </form>
+        
+    )
 }
