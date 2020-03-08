d83a57b2fe7d6bc45ff1f666f8f07162bfcd5819
Érick Firmo <erickluques@gmail.com>
2020-03-08 18:14:40 -0300

    Update routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const routes = express.Router();
 
+
+routes.post('/users', (req, res) => {
+    return res.json(req.body);
+});
+
+
 modules.exports =  routes;
