5e5ed24bb5f524fc17e82192022a0c27e2bae2c2
Érick Firmo <erickluques@gmail.com>
2020-03-08 18:14:44 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,8 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
-
-app.post('/users', (req, res) => {
-    return res.json(req.body);
-});
 
 app.listen(3333);
